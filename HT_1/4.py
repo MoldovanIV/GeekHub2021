21ff641c3317af0f19947cd955d476b90263ec0e
MoldovanIV <moldovan.iv2016@gmail.com>
2021-11-18 17:59:43 +0200

    Write a script to concatenate N strings.

--- HT_1/4.py
+++ HT_1/4.py
@@ -1,10 +1,13 @@
 n=int(input("Введіть кількість рядків N="))
-print("Введіть 1 рядок: ")
-conc_s=input()
-i=2
-while i<=n:
-    print("Введіть ",i," рядок: ")
-    s=input()
-    conc_s=conc_s+s
-    i=i+1
-print(conc_s)
+if n>0:
+    print("Введіть 1 рядок: ")
+    conc_s=input()
+    i=2
+    while i<=n:
+        print("Введіть ",i," рядок: ")
+        s=input()
+        conc_s=conc_s+s
+        i=i+1
+    print(conc_s)
+else:
+    print("Ви ввели некоректну кількість рядків")
