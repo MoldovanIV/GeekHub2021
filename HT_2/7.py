d065199bb2e3538fc9dd466702f7882b512d7eb6
MoldovanIV <moldovan.iv2016@gmail.com>
2021-11-20 21:54:02 +0200

    Отримати максимальне і мінімальне значення із словника.
     Дані захардкодити

--- HT_2/7.py
+++ HT_2/7.py
@@ -1,6 +1,7 @@
+#Отримати максимальне і мінімальне значення із словника. Дані захардкодити.
 d = {0: -100, 1: 10, 2: 30, 3: 50, 4: 70, 5: 90}
 print("Словник:\n",d)
-key_max = max(d.keys(), key=(lambda k: d[k]))
-key_min = min(d.keys(), key=(lambda k: d[k]))
+key_max = max(d, key=d.get)
+key_min = min(d, key=d.get)
 print('Max: ',d[key_max])
 print('Min: ',d[key_min])
