469034657821f5d348933e856265c463ea266674
MoldovanIV <moldovan.iv2016@gmail.com>
2021-11-23 23:35:03 +0200

    Калькулятор
    Повинна бути 1 ф-цiя яка б приймала 3 аргументи - один з яких операцiя, яку зробити!

---
+++ HT_3/7.py
@@ -0,0 +1,18 @@
+#Калькулятор :)
+#повинна бути 1 ф-цiя яка б приймала 3 аргументи - один з яких операцiя, яку зробити!
+def calcul(a,oper,b):
+    if oper == '+':
+        rez=a+b
+    elif oper == '-':
+        rez=a-b
+    elif oper == '*':
+        rez=a*b
+    elif oper == '/':
+        rez=a/b
+    else:
+        rez=input("Помилка у введенні позначення дії")
+    return rez  
+x = float(input("Введіть перший аргумент х = "))
+znak = input("Введіть позначення дії ( +, -, *, / ) = ")
+y = float(input("Введіть другий аргумент y = "))
+print(f"{x} {znak} {y} = {calcul(x,znak,y)}")
