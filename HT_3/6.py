d6c5c2f0b95dc388f2b1c8c8a6da7a2c5859a94d
MoldovanIV <moldovan.iv2016@gmail.com>
2021-11-24 22:39:25 +0200

    Маємо рядок
     Створіть ф-цiю, яка буде отримувати рядки на зразок цього, яка оброблює наступні випадки

--- HT_3/6.py
+++ HT_3/6.py
@@ -1,4 +1,3 @@
-# Тут мені потрібна допомога
 #Маємо рядок --> "f98neroi4nr0c3n30irn03ien3c0rfekdno400wenwkowe00koijn35pijnp46ij7k5j78p3kj546p465jnpoj35po6j345" -> просто потицяв по клавi
 #   Створіть ф-цiю, яка буде отримувати рядки на зразок цього, яка оброблює наступні випадки:
 #-  якщо довжина рядка в діапазонi 30-50 -> прiнтує довжину, кiлькiсть букв та цифр
@@ -17,12 +16,12 @@ def magic (s):
         for symb in s:
             if symb.isdigit():
                 suma += int(symb)
-            rez1 = str(print(f"Сума цифр = {suma}"))
+        rez1 = str(print(f"Сума цифр = {suma}"))
         b_list = ''
         for symb in s:
             if symb.isalpha():
                 b_list.append(symb)
-            rez2 = str(print(f"Лише букви: {b_list}"))
+        rez2 = str(print(f"Лише букви: {b_list}"))
         rez = rez1 + rez2
     else:
         rez = 'Вам пора відпочити!'
