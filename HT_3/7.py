ae26913b1e13cada686e225567240aa29e387eb9
MoldovanIV <moldovan.iv2016@gmail.com>
2021-11-25 16:02:23 +0200

    Калькулятор

--- HT_3/7.py
+++ HT_3/7.py
@@ -1,21 +1,21 @@
 #Калькулятор :)
 #повинна бути 1 ф-цiя яка б приймала 3 аргументи - один з яких операцiя, яку зробити!
 def calcul(a,oper,b):
-    if oper == '+':
-        rez=a+b
-    elif oper == '-':
-        rez=a-b
-    elif oper == '*':
-        rez=a*b
-    elif oper == '/':
-        rez=a/b
+    if b == 0 and oper == '/':
+        rez=input("Ділити на нуль не можна!")
     else:
-        rez=input("Помилка у введенні позначення дії")
+        if oper == '+':
+            rez=a+b
+        elif oper == '-':
+            rez=a-b
+        elif oper == '*':
+            rez=a*b
+        elif oper == '/':
+            rez=a/b
+        else:
+            rez=input("Помилка у введенні позначення дії або ділення на нуль")
     return rez  
 x = float(input("Введіть перший аргумент х = "))
 znak = input("Введіть позначення дії ( +, -, *, / ) = ")
 y = float(input("Введіть другий аргумент y = "))
-if (y == 0) and (znak =='/'):
-    print("Ділити на 0 не можна!")
-else:    
-    print(f"{x} {znak} {y} = {calcul(x,znak,y)}")
+print(f"{x} {znak} {y} = {calcul(x,znak,y)}")
