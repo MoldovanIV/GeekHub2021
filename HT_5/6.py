0c030e9a77ce6fd406caffd87bef6ef06f0a64da
MoldovanIV <moldovan.iv2016@gmail.com>
2021-12-01 20:14:13 +0200

    Всі ви знаєте таку функцію як <range>. Напишіть свою реалізацію цієї функції.

--- HT_5/6.py
+++ HT_5/6.py
@@ -1,9 +1,43 @@
 #Всі ви знаєте таку функцію як <range>. Напишіть свою реалізацію цієї функції.
-def iterate(a, b, step=1):
-    x = 0
-    while abs(x) < abs(b-a):
-        yield a+x
-        x += step
-print(type(iterate(20, 2, -2)))
-for i in iterate(20, 2, -2):
+def iterate(*args):
+    try:
+        if len(args) == 1:
+            a = 0
+            b = float(args[0])
+            step = 1
+        elif len(args) == 2:
+            a = float(args[0])
+            b = float(args[1])
+            step = 1
+        elif len(args) == 3:
+            a = float(args[0])
+            b = float(args[1])
+            step = float(args[2])
+            if step == 0:
+                raise ValueError("Крок не може дорівнювати 0")
+        else:
+            raise TypeError("Надто багато аргументів")
+    except ValueError as e:
+        print(e)
+        return
+    except TypeError:
+        print("Генератор приймає від 1 до 3 аргументів!")
+        return
+
+    if a <= b and step>=0:
+        x = a
+        while x < b:
+            yield x
+            x += step
+    elif b <= a and step<0:
+        x = a
+        while x > b:
+            yield x
+            x += step
+    else:
+        yield ""
+
+print(type(iterate(1, 10, 2)))
+
+for i in iterate(12,2,-2):
     print(i)
