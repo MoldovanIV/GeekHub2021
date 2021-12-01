9ec765930385571a330611cc0fe588d6255b2fd3
MoldovanIV <moldovan.iv2016@gmail.com>
2021-12-01 14:38:59 +0200

    Створіть функцію для валідації пари ім'я/пароль за наступними правилами
    - ім'я повинно бути не меншим за 3 символа і не більшим за 50;
       - пароль повинен бути не меншим за 8 символів і повинен мати хоча б одну цифру;
       - щось своє :)
       Якщо якийсь із параметрів не відповідає вимогам - породити виключення із відповідним текстом.

---
+++ HT_5/2.py
@@ -0,0 +1,26 @@
+#Створіть функцію для валідації пари ім'я/пароль за наступними правилами:
+#   - ім'я повинно бути не меншим за 3 символа і не більшим за 50;
+#   - пароль повинен бути не меншим за 8 символів і повинен мати хоча б одну цифру;
+#  - щось своє :)
+#   Якщо якийсь із параметрів не відповідає вимогам - породити виключення із відповідним текстом.
+import re
+pattern = r'[A-Z]{2}'
+class LoginException(Exception):
+    pass
+
+def validate(login, password):
+    contains_digit = False
+    for character in password:
+        if character.isdigit():
+            contains_digit = True
+
+    has_2capital_letters = re.search(pattern, password)
+    if 3 <= len(login) <= 50 and len(password) >= 8 and contains_digit == True and has_2capital_letters is not None:
+        return "Вітаємо, Ви авторизовані!"
+    else:
+        raise LoginException("Проблема авторизації!")
+log = input("Введіть логін: ")
+passw = input("Введіть пароль: ")
+print(validate(log,passw))
+
+
