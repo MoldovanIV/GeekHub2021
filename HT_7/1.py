18bf6009c1f0d99fdc48577063ba1c0ba7e67411
MoldovanIV <moldovan.iv2016@gmail.com>
2021-12-12 23:31:29 +0200

    +правки

--- HT_7/1.py
+++ HT_7/1.py
@@ -1,48 +1,87 @@
+#Програма-банкомат
 import json
+import csv
 
-base = {"Ivanov" : "12345", "Bobov" : "808bo", "Hrek" : "yyy111"}
+VISITOR = ''
 
-#Створення текстового файла з даними користувачів (логін/пароль)
+def login_password():
+    global VISITOR
+    attempt = 3
+    while attempt:
+        print(f'Ви маєте {attempt} спроби\n')
+        login, password = map(str, input('Введіть логін та пароль (використовуючи пробіл): ').split())
+        with open('users.csv') as f:
+            reader = csv.reader(f)
+            reader = next(reader)
+            for row in reader:
+                if login == row[0] and password == row[1]:
+                    visitor = login
+                    return True
+        attempt -= 1
+        return '3 спроби використано'
+#працює
+def menu():
+    print('Вітаємо!\n')
+    print('Переглянути баланс, натисніть 1\n')
+    print('Поповнити баланс, ннатисніть 2\n')
+    print('Зняти готівку, натисніть 3\n')
+    print('Для виходу, натисніть 4\n')
+    user_action = int(input())
+    return user_action
 
-try:
-    with open("users.txt","w") as file:
-        for login,parol in base.items():
-            file.write("{}:{}".format(login,parol))
-except FileNotFoundError:
-    print("Неможливо відкрити файл")
-finally:
-    print(file.closed)
+#не працює файл
+def view_balance():
+    with open(VISITOR+'_balance.csv') as balance:
+        print(f'Ваш баланс: {balance.read()}')
+    print('--------------')
 
-#Фукнція start()
-    
-def start(user_login,user_parol,base):
-    if user_login in base.keys() and user_parol == base[user_login]:
-        operation = int(input("Яку дію бажаєте виконати?\n-------------------------------\nПереглянути баланс - Натисніть 1\nПоповнити баланс - Натисніть 2\nЗняти готівку - Натисніть 3\nВихід - Натисніть 4\n"))
-        if operation == 1:
-            print("Ваш баланс становить: ")
-        elif operation == 2:
-            print("Введіть суму поповнення: ")
-        elif operation == 3:
-            print("Введіть суму, яку бажаєте зняти: ")
-        else:
-            print ("Вже йдете? Так швидко? :( Приходьте ще!\nГарного Вам дня!")
+#не працює файл, та сама помилка з visitor
+def ad_balance():
+    add_balance = int(input('Введіть суму поповнення: '))
+    if add_balance < 0:
+      return 'Помилка! Введено від\'ємну суму!'
+    with open(VISITOR+'_balance.csv') as balance:
+        b = balance.read()
+    with open(VISITOR+'_balance.csv', 'w') as balance:
+        balance.write(str(add_balance + int(b)))
+    with open(VISITOR+'_balance.csv') as balance, open(VISITOR+'_transactions.csv', 'a') as transactions:
+        b = balance.read()
+        transactions.write(json.dumps(b))
+        transactions.write('\n')
+    print('--------------')
+
+#не працює файл, та сама помилка з visitor
+def subt_balance():
+    subtract_balance = int(input('\nВведіть суму, яку бажаєте зняти: '))
+    with open(f'{VISITOR}_balance.csv') as balance:
+        b = balance.read()
+    if subtract_balance < 0:
+      return 'Введено від\'ємну суму'
+    if int(b) >= subtract_balance:
+        with open(VISITOR+'_balance.csv', 'w') as balance:
+            balance.write(str(int(b) - subtract_balance))
+        with open(VISITOR+'_balance.csv') as balance, open(VISITOR+'_transactions.csv', 'a') as transactions:
+            b = balance.read()
+            transactions.write(json.dumps(b))
+            transactions.write('\n')
+        print('Операція успішна!')
     else:
-        print("Упс... Дані введено не вірно. Спробуйте ще.")
+        print('Недостатньо коштів...')
+    print('--------------')
 
-#   def balance
-#def balance(user_login):
-    
-#   def transactions
-def transactions(user_login):
-    tran = []
-    base_tran = "tran.json"
-    with open(base_tran,"w") as tr:
-        json.dump(tran,tr)
+#працює
+def start():
+    if login_password():
+        while True:
+            result = menu()
+            if result == 1:
+                view_balance()
+            if result == 2:
+                ad_balance()
+            if result == 3:
+                subt_balance()
+            if result == 4:
+                return 'До побачення!'
+    return 'До побачення!'
 
-print("Доброго дня!")
-
-us_l = input("Введіть Ваш логін: ")
-us_p = input("Введіть Ваш пароль: ")
-
-start(us_l,us_p,base)
-transactions(us_l)
+start()
